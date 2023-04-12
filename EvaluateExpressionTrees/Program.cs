78b600bef00061fb10baaf474a04ea6bb37593c5
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-04-12 22:09:57 +0200

    Evaluate Expression Trees - initializing

---
+++ EvaluateExpressionTrees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvaluateExpressionTrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
