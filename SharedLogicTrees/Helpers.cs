4bc60ce79222c3eae67d8a9c95949ed52b7554f6
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-05-11 00:43:57 +0200

    Edit print tree - Symmetric Tree initiation

--- SharedLogicTrees/Helpers.cs
+++ SharedLogicTrees/Helpers.cs
@@ -10,13 +10,13 @@ namespace SharedLogicTrees
 		{
 			var q = new Queue<(BinaryTree node, int depth)>();
 			q.Enqueue((root, 0));
-			Dictionary<int, int> nodesValueDepth = new Dictionary<int, int>();
+            List<(int, int)> nodesValueDepth = new List<(int, int)>();
 
-			while (q.Count > 0)
+            while (q.Count > 0)
 			{
 				var currentNode = q.Dequeue();
 				var currentDepth = currentNode.depth + 1;
-				nodesValueDepth.Add(currentNode.node.value, currentNode.depth);
+				nodesValueDepth.Add((currentNode.node.value, currentNode.depth));
 				if (currentNode.node.left != null)
 				{
 					q.Enqueue((currentNode.node.left, currentDepth));
@@ -27,14 +27,14 @@ namespace SharedLogicTrees
 				}
 			}
 
-			var maxDepth = nodesValueDepth.Values.Max();
+			var maxDepth = nodesValueDepth.Select(a=>a.Item2).Max();
 			for (int i = 0; i <= maxDepth; i++)
 			{
 				if (i > 0)
 				{
 					Console.WriteLine("|");
 				}
-				Console.WriteLine(string.Join('-', nodesValueDepth.Where(a => a.Value == i).Select(a => a.Key).ToList()));
+				Console.WriteLine(string.Join('-', nodesValueDepth.Where(a => a.Item2 == i).Select(a => a.Item1).ToList()));
 			}
 		}
 	}
---
+++ SymmetricalTree/Program.cs
@@ -0,0 +1,50 @@
+﻿using SharedLogicTrees;
+using System;
+
+
+namespace SymmetricalTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Case__1
+            BinaryTree tree1 = new BinaryTree(1);
+            BinaryTree tree2 = new BinaryTree(2);
+            BinaryTree tree3 = new BinaryTree(2);
+            BinaryTree tree4 = new BinaryTree(3);
+            BinaryTree tree5 = new BinaryTree(4);
+            BinaryTree tree6 = new BinaryTree(4);
+            BinaryTree tree7 = new BinaryTree(3);
+            BinaryTree tree8 = new BinaryTree(5);
+            BinaryTree tree9 = new BinaryTree(6);
+            BinaryTree tree10 = new BinaryTree(6);
+            BinaryTree tree11 = new BinaryTree(5);
+
+            tree1.left = tree2;
+            tree1.right = tree3;
+
+            tree2.left = tree4;
+            tree2.right = tree5;
+
+            tree3.left = tree6;
+            tree3.right = tree7;
+
+            tree4.left = tree8;
+            tree4.right = tree9;
+
+            tree7.left = tree10;
+            tree7.right = tree11;
+            #endregion
+
+            Helpers.PrintTree(tree1);
+            Console.WriteLine($"is this tree Symmetrical : {SymmetricalTree(tree1)}");
+        }
+
+        public static bool SymmetricalTree(BinaryTree tree)
+        {
+            // Write your code here.
+            return false;
+        }
+    }
+}
